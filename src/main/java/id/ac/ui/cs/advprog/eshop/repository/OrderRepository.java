71fbd7eeb405cb8a15bc99f9f9fdca28b0564706
nandanathaniela <124995047+nandanathaniela@users.noreply.github.com>
2024-03-05 15:42:41 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -5,7 +5,36 @@ import java.util.List;
 
 public class OrderRepository {
     private List<Order> orderData = new ArrayList<>();
-    public Order save(Order order) {return null;}
-    public Order findById(String id) {return null;}
-    public List<Order> findAllByAuthor(String author) {return null;}
+    public Order save(Order order) {
+        int i = 0;
+        for (Order o : orderData) {
+            if (o.getId().equals(order.getId())) {
+                orderData.set(i, order);
+                return order;
+            }
+            i++;
+        }
+        orderData.add(order);
+        return order;
+    }
+    public Order findById(String id) {
+        for (Order o : orderData) {
+            if (o.getId().equals(id)) {
+                return o;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Order> findAllByAuthor(String author) {
+        List<Order> result = new ArrayList<>();
+        for (Order o : orderData) {
+            if (o.getAuthor().equals(author)) {
+                result.add(o);
+            }
+        }
+
+        return result;
+    }
 }
