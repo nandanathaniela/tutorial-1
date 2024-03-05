d78b550e28c04201cae1b89c1b66802be42185bd
nandanathaniela <124995047+nandanathaniela@users.noreply.github.com>
2024-03-05 18:33:36 +0700

    [REFACTOR] Fix beans Autowired

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -7,13 +7,20 @@ import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.model.VoucherPayment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.UUID;
-
+@Service
 public class PaymentServiceImpl implements PaymentService {
-    private PaymentRepository paymentRepository;
+    private final PaymentRepository paymentRepository;
+    @Autowired
+    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> data) {
         Payment payment;
